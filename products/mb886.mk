26326828fc31a05c3c2b003bf12bc2e1efca5e0d
skeevy420 <r.allen.payne@gmail.com>
2013-07-07 01:54:05 -0500

    change qinara to mb886

--- products/mb886.mk
+++ products/mb886.mk
@@ -1,5 +1,5 @@
 # Inherit AOSP device configuration for maguro.
-$(call inherit-product, device/motorola/qinara/full_qinara.mk)
+$(call inherit-product, device/motorola/mb886/full_mb886.mk)
 
 # Inherit GSM common stuff
 $(call inherit-product, vendor/rootbox/configs/gsm.mk)
@@ -14,14 +14,14 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/rootbox/overlay/moto-qcom-common
 OVERLAY_TARGET := pa_xhdpi
 
 # Setup device specific product configuration.
-PRODUCT_NAME := rootbox_qinara
+PRODUCT_NAME := rootbox_mb886
 PRODUCT_BRAND := Motorola
-PRODUCT_DEVICE := qinara
+PRODUCT_DEVICE := mb886
 PRODUCT_MODEL := MB886
 PRODUCT_MANUFACTURER := Motorola
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=qinara BUILD_FINGERPRINT=motorola/mb886/qinara:4.2.2/JDQ39/346380647:user/release-keys PRIVATE_BUILD_DESC="qinara-user 4.2.2 JDQ39 346380647 release-keys" BUILD_NUMBER=346380647
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=mb886 BUILD_FINGERPRINT=motorola/mb886/qinara:4.2.2/JDQ39/346380647:user/release-keys PRIVATE_BUILD_DESC="qinara-user 4.2.2 JDQ39 346380647 release-keys" BUILD_NUMBER=346380647
 
 # Qinara specific packages
 #PRODUCT_PACKAGES += \
